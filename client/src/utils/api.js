2db13ba52cbbd39f31f0ee9a00f4ecb398b735c6
Animesh <ajjain21022002@gmail.com>
2024-11-13 23:14:08 +0530

    Updated url 3

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const newRequest = axios.create({
-	baseURL: 'https://creators-hub-ik2d.onrender.com/',
+	baseURL: 'https://creators-hub-ik2d.onrender.com/api/',
 	withCredentials: true,
 	// Allows to store cookies
 	timeout: 10000,
