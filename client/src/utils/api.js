3d3b28874d9b458f7069057675611b419cb95f49
Animesh <ajjain21022002@gmail.com>
2024-11-13 23:00:59 +0530

    Updated urls

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,11 +1,22 @@
 import axios from 'axios';
 
 const newRequest = axios.create({
-	baseURL: 'http://localhost:8080/api',
+	baseURL: 'https://creators-hub-ik2d.onrender.com/',
 	withCredentials: true,
-	// timeout: 10000,
 	// Allows to store cookies
+	timeout: 10000,
 	// TODO : search about this
 });
+
+// Fallback url logic
+newRequest.interceptors.response.use(
+	(response) => response,
+	async (error) => {
+		if (error.code === 'ECONNABORTED') {
+			error.config.baseURL = 'http://localhost:8080';
+			return axios.request(error.config);
+		}
+	}
+);
 
 export default newRequest;
